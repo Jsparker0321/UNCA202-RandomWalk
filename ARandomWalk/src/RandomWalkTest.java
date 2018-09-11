aba824b4df1864ec18244c92c837b56d6ce74fc4
jessp <jessp@10.120.188.224>
2018-09-10 22:22:16 -0400

    formatted output

--- ARandomWalk/src/RandomWalkTest.java
+++ ARandomWalk/src/RandomWalkTest.java
@@ -1,31 +1,54 @@
+import java.util.Scanner;
 
 public class RandomWalkTest {
 
 	public static void main(String[] args) {
-	
 
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("Enter user name: ");
+		String name = userInput.nextLine();
+		System.out.println("");
+		System.out.println("Welcome, " + name + "!");
+		System.out.println("------------------------");
+
+		System.out.println("Bug Map #1: ");
 		RandomWalk simulationOne = new RandomWalk();
+		simulationOne.runBug();
+		System.out.println("It took " + simulationOne.getTotalSteps() + " steps to fill the grid!");
+		System.out.println("");
 		
-			simulationOne.runBug();
-			System.out.println(simulationOne.getTotalSteps());
-			
-		
-			
-		
+		System.out.println("Bug Map #2: ");
 		RandomWalk simulationTwo = new RandomWalk();
-		
-			simulationTwo.runBug();
-			System.out.println(simulationTwo.getTotalSteps());
-		
+		simulationTwo.runBug();
+		System.out.println("It took " + simulationTwo.getTotalSteps() + " steps to fil the grid!");
+		System.out.println("");
+
+		System.out.println("Bug Map #3: ");
 		RandomWalk simulationThree = new RandomWalk();
-		
-			simulationThree.runBug();
-			System.out.println(simulationThree.getTotalSteps());
-		
+		simulationThree.runBug();
+		System.out.println("It took " + simulationThree.getTotalSteps() + " steps to fill the grid!");
+		System.out.println("");
+
+		System.out.println("Bug Map #4: ");
 		RandomWalk simulationFour = new RandomWalk();
+		simulationFour.runBug();
+		System.out.println("It took " + simulationFour.getTotalSteps() + " steps to fill the grid!");
+		System.out.println("");
 		
-			simulationFour.runBug();
-			System.out.println(simulationFour.getTotalSteps());
+		
+		
+		System.out.print("Would you like to simulate another map? ");
+		String answer = userInput.nextLine();
+		if( answer.equals("yes")) {
+			System.out.println("Bug Map #5: ");
+			RandomWalk simulationFive = new RandomWalk();
+			simulationFive.runBug();
+			System.out.println("It took " + simulationFive.getTotalSteps() + " steps to fill the grid!");
+			System.out.println("");
+		}else {
+			System.out.println("Goodbye!");
+		}
+		
 	}
 
 }
